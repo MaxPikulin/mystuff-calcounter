f3b9c2db385220e20064ed410ac64c550149fb4c
MaxPikulin <trurl1000@gmail.com>
2017-09-30 20:30:25 +0100

    v0.4
    on press enter focus on next input or create new line. bigger font.

--- script.js
+++ script.js
@@ -1,44 +1,90 @@
 const plusBtn = document.getElementsByClassName('plusbtn')[0];
 const mainWindow = document.getElementsByClassName('mainwindow')[0];
 const totalDiv = document.getElementsByClassName('total')[0];
+// let rowCount = 0;
+let fieldsObj = {};
 
-function addRow (){
-  const row = document.createElement('div');
-  row.innerHTML = `
-  <div class="rows">
-    <input type="text" class="name"><input type="text" class="per100"><input type="text" class="grams"><div class="result"></div>
-  </div>`;
-  mainWindow.insertBefore(row, plusBtn);
+function storeData() {
+  localStorage.setItem('current', JSON.stringify(fieldsObj));
 }
-function handleChange(e) {
-  let per100 = parseInt(e.target.parentNode.children[1].value);
-  let weight = e.target.parentNode.children[2].value;
-  let text = e.target.parentNode.children[3];
 
-  if(weight.includes('-')) {
-    weight = weight.split('-').reduce((a,v)=>+a- +v);
+function restoreData() {
+  fieldsObj = localStorage.getItem('current');
+  for (let row in fieldsObj) {
+    addRow(row);
+  }
+}
+
+function addRow(rowData) {
+  // rowCount++;
+  //debugger;
+  const {
+    num = '',
+      name = '',
+      cal = '',
+      weight = '',
+      result = ''
+  } = (rowData || {});
+  let row = document.createElement('div');
+  row.classList.add('rows');
+  row.id = num;
+  row.innerHTML = `
+    <input type="text" class="name" value="${name}"><input type="text" class="cal" value="${cal}"><input type="text" class="weight" value="${weight}"><div class="result">${result}</div><button name="delrowbtn" class="delrow">X</button>
+`;
+  mainWindow.insertBefore(row, plusBtn);
+  row.children[0].focus();
+}
+
+function handleChange(e) {
+  console.log(e);
+  let target = e.target;
+  let parent = target.parentNode;
+  let cal = parent.getElementsByClassName('cal')[0].value;
+  let weight = parent.getElementsByClassName('weight')[0].value;
+  let result = parent.getElementsByClassName('result')[0];
+
+
+  if (e.which == 13) {
+    if (target.classList.value.includes('weight')) {
+      addRow();
+    }
+    target.nextSibling.focus();
+  }
+
+  if (weight.includes('-')) {
+    weight = weight.split('-').reduce((a, v) => +a - +v);
   } else {
     weight = parseInt(weight);
   }
 
-  let result = per100*weight/100;
-  if(isNaN(result)) {
-    text.textContent = '';
+  let calc = cal * weight / 100;
+  if (isNaN(calc)) {
+    result.textContent = '';
     total();
     return;
   }
-  text.textContent = result.toFixed(2);
+  result.textContent = calc.toFixed(2);
   total();
+  //store();
 }
+
+function handleClick(e) {
+  if (e.target.name != "delrowbtn") return;
+  e.target.parentNode.remove();
+  rowCount--;
+}
+
 function total() {
   let allResults = document.querySelectorAll('.result');
   let total = 0;
-  allResults.forEach(node=>total+= +node.textContent);
+  allResults.forEach(node => total += +node.textContent);
   totalDiv.textContent = total.toFixed(2);
 }
 
 addRow();
+//restore();
 
-plusBtn.addEventListener('click',addRow);
-mainWindow.addEventListener('change',handleChange);
-mainWindow.addEventListener('keyup',handleChange);
+plusBtn.addEventListener('click', addRow);
+mainWindow.addEventListener('change', handleChange);
+mainWindow.addEventListener('keyup', handleChange);
+mainWindow.addEventListener('click', handleClick);
