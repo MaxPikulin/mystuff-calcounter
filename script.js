3bda6fdf118d9f326748804c29cc4f70e7fdf258
MaxPikulin <trurl1000@gmail.com>
2017-10-13 20:02:02 +0100

    wip

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// test some stuff
+// global vars
+const mainWindow = document.getElementById('mainwindow');
+const addRowBtn = document.getElementById('addrow');
+const totalDiv = document.getElementById('total');
+const recipeObject = {};
+
 function storeData() {
   // TODO: Store fields data in object and save it to localstorage.
 }
@@ -20,13 +25,23 @@ function calculateTotal() {
 }
 
 function addRow() {
-  // TODO: add new row to the screen.
+  // TODO: add new row to the screen. (looks like innerHTML works faster in this case than bunch of createElements and classList.add)
+  const row = document.createElement('div');
+  row.innerHTML = `<input class="itemName"><input class="calories"><input class="weight"><div class="result"></div>`;
+  mainWindow.appendChild(row);
+  return row;
 }
 
 function changeHandler(e) {
   // TODO: handle all change events on mainform, pass needed references to other functions (saveRecipeObject...)
+
 }
 
 function removeRow() {
   // TODO: remove row.
 }
+
+
+// event handlers
+addRowBtn.addEventListener('click', addRow);
+mainWindow.addEventListener('change', changeHandler);
